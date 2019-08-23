221ee6a16e3f6e24adbdda9618ecfa3a2f5ea742
xin <5ujiedai@gmail.com>
2019-08-23 14:27:12 -0400

    Added TableViewer Construct library to expose hitcounter table view

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigateway = require('@aws-cdk/aws-apigateway');
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -19,7 +20,12 @@ export class CdkWorkshopStack extends cdk.Stack {
     });
 
     new apigateway.LambdaRestApi(this, 'Endpoint', {
-      handler: helloWithCounter.handler
+      handler: helloWithCounter.handler //in HitCounter class, handler is public readonly
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
   }
 }
