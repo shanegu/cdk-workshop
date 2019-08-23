d06ee9ac8a70626382fe3b273e1a6a2867c8b1aa
xin <5ujiedai@gmail.com>
2019-08-23 15:04:37 -0400

    Sort the HitCounter table view by hits in desc order

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -25,7 +25,8 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
-      table: helloWithCounter.table
+      table: helloWithCounter.table,
+      sortBy: '-hits'
     });
   }
 }
